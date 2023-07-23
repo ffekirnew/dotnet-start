85fec777f50447a5feffabfe337f96c626f64a2f
ffekirnew <ffekirnew0808@gmail.com>
2023-07-23 20:42:29 +0300

    Work on buber-breakfast from YouTube

---
+++ BuberBreakfast/BuberBreakfast/Controllers/BreakfastsController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Contracts;
+using BuberBreakfast.Contracts.Breakfast;
+using BuberBreakfast.Models;
+using BuberBreakfast.Services.Breakfasts;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreakfastsController : ControllerBase
+{
+    private readonly IBreakfastService _breakfastService;
+
+    public BreakfastsController(IBreakfastService breakfastService)
+    {
+        _breakfastService = breakfastService;
+    }
+
+    [HttpPost]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        var breakfast = new Breakfast(
+            Guid.NewGuid(),
+            request.Name,
+            request.Description,
+            request.StartDateTime,
+            request.EndDateTime,
+            DateTime.UtcNow,
+            request.Savory,
+            request.Sweet
+            );
+        
+        _breakfastService.CreateBreakfast(breakfast);
+        
+        var response = new BreakfastResponse(
+            breakfast.Id,
+            breakfast.Name,
+            breakfast.Description,
+            breakfast.StartDateTime,
+            breakfast.EndDateTime,
+            breakfast.LastModifiedDateTime,
+            breakfast.Savory,
+            breakfast.Sweet
+            );
+
+        return CreatedAtAction(
+            actionName: nameof(GetBreakfast),
+            routeValues: new { id = response.Id },
+            value: response);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        var breakfast = _breakfastService.GetBreakfast(id);
+        return Ok(breakfast);
+    }
+    
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+}
---
+++ BuberBreakfast/BuberBreakfast/Services/Breakfasts/BreakfastService.cs
@@ -0,0 +1,23 @@
+using BuberBreakfast.Models;
+
+namespace BuberBreakfast.Services.Breakfasts;
+
+public class BreakfastService : IBreakfastService
+{
+    private static readonly Dictionary<Guid, Breakfast> Breakfasts = new();
+
+    public void CreateBreakfast(Breakfast breakfast)
+    {
+        Breakfasts.Add(breakfast.Id, breakfast);
+    }
+
+    public Breakfast GetBreakfast(Guid id)
+    {
+       return Breakfasts[id];
+    }
+
+    public Breakfast DeleteBreakfast(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
